006a2d5451d6942f14e82de18fea3bef3d4d9ce9
Administrator <Administrator@DESKTOP-CV6TJJL>
2025-08-14 08:59:54 +0200

    add new code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hossam");
+Console.WriteLine("C#");
+Console.WriteLine("C#");
